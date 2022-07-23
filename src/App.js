4efa70267be0f26b7b73d6944386a73e441c2fdd
Harsh <valambeharsh1701@gmail.com>
2022-07-24 03:02:09 +0530

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
+import Fetchnews from './componets/fetchnews';
 import Home from './componets/home';
 
 function App() {
   return (
     <>
     <Home/>
-    
+    <Fetchnews/>
+
     </>
   );
 }
--- src/componets/fetchnews.jsx
+++ src/componets/fetchnews.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import axios from 'axios'
 const Fetchnews = () => {
-  return (
-    <div>F</div>
-  )
+    const [news,setNews]=useState([])
+    const fetchNews=()=>{
+        // console.log("clicked");
+        axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=2370b45295f440c59b98c8f36d21079e")
+        .then((response)=>{
+            console.log(response);
+        })
+    }
+    return (
+        <>
+            <div className="flex">
+                <button className='bg-blue-300 text-black text-xl p-3 rounded-md' onClick={fetchNews}>Fetch news </button>
+            </div>
+        </>
+    )
 }
 
 export default Fetchnews
